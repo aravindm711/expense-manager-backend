31313a6023ea36e8de0336fb081e662a0a98f183
Aravind Murali <aravindmurali711@gmail.com>
2021-04-12 15:34:49 +0530

    modified views for better json responses

--- em/api/views.py
+++ em/api/views.py
@@ -56,9 +56,9 @@ class ExpenseViewSet(ModelViewSet):
         return Expense.objects.filter(created_by=self.request.user).order_by("-date")
 
     def create(self, request, *args, **kwargs):
-        data = copy.deepcopy(request.data)
-        data["created_by"] = get_user_model().objects.get(username=request.user).pk
-        serializer = self.get_serializer(data=data)
+        # data = copy.deepcopy(request.data)
+        # data["created_by"] = get_user_model().objects.get(username=request.user).pk
+        serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
@@ -100,10 +100,27 @@ class LentOrOwedExpenseViewSet(ModelViewSet):
             Q(created_by=self.request.user) | Q(to=self.request.user)
         )
 
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        for i in range(len(serializer.data)):
+            serializer.data[i]["created_by"] = (
+                get_user_model()
+                .objects.get(pk=serializer.data[i]["created_by"])
+                .username
+            )
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
     def create(self, request, *args, **kwargs):
         if request.data["to"] != str(request.user):
-            data = copy.deepcopy(request.data)
             created_by = get_user_model().objects.get(username=request.user)
+            data = copy.deepcopy(request.data)
             data["created_by"] = created_by.pk
             serializer = self.get_serializer(data=data)
             serializer.is_valid(raise_exception=True)
@@ -111,20 +128,59 @@ class LentOrOwedExpenseViewSet(ModelViewSet):
             headers = self.get_success_headers(serializer.data)
 
             if data["amount"] >= 0:
-                det = "{} owed to {}".format(data["amount"], created_by)
+                det = "{} owed to {} for {}".format(
+                    data["amount"], created_by, data["detail"]
+                )
             else:
-                det = "{} lent to {}".format(data["amount"], created_by)
+                det = "{} lent to {} for {}".format(
+                    data["amount"], created_by, data["detail"]
+                )
+            if data["detail"] == "settle":
+                det = "Settled {} to {}".format(data["amount"], created_by)
             to = get_user_model().objects.get(username=data["to"])
-            new_notif = ExpenseNotification.objects.create(to=to, detail=det)
+            ExpenseNotification.objects.create(to=to, detail=det)
+
+            data = copy.deepcopy(serializer.data)
+            data.update({'created_by': str(request.user)})
 
             return Response(
-                {**serializer.data}, status=status.HTTP_201_CREATED, headers=headers
+                {**data}, status=status.HTTP_201_CREATED, headers=headers
             )
         else:
             return Response(status=status.HTTP_400_BAD_REQUEST)
 
     def perform_create(self, serializer):
         serializer.save(created_by=self.request.user)
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        created_by = get_user_model().objects.get(username=request.user)
+        amount_before = instance.amount
+        data = copy.deepcopy(request.data)
+        data["created_by"] = get_user_model().objects.get(username=request.user).pk
+        serializer = self.get_serializer(instance, data=data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        headers = self.get_success_headers(serializer.data)
+
+        det = "{} updated to {} by {}".format(amount_before, data["amount"], created_by)
+        to = get_user_model().objects.get(username=data["to"])
+        ExpenseNotification.objects.create(to=to, detail=det)
+
+        data = copy.deepcopy(serializer.data)
+        data.update({'created_by': str(request.user)})
+
+        return Response(
+            {**serializer.data}, status=status.HTTP_202_ACCEPTED, headers=headers
+        )
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if instance.to != str(request.user):
+            instance.delete()
+            return Response(status=status.HTTP_200_OK)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
 class ExpenseNotificationViewSet(ModelViewSet):
@@ -137,8 +193,10 @@ class ExpenseNotificationViewSet(ModelViewSet):
 
     def update(self, request, *args, **kwargs):
         instance = self.get_object()
-        data = copy.deepcopy(request.data)
+        data = {}
         data["to"] = get_user_model().objects.get(username=request.user).pk
+        data["notified"] = "READ"
+        data["detail"] = instance.detail
         serializer = self.get_serializer(instance, data=data)
         serializer.is_valid(raise_exception=True)
         self.perform_update(serializer)
